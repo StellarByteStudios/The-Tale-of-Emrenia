6c811bd4f14b020406d1bfd33b26baf893aad6f5
Lord-Emreno <emre.uestuen33mail.com>
2021-05-28 21:45:10 +0200

    Main Methode geupdatet

--- The Tale of Emrenia/src/main/java/mainGame/Main.java
+++ The Tale of Emrenia/src/main/java/mainGame/Main.java
@@ -1,8 +1,13 @@
 package mainGame;
 
+
 public class Main {
+
+    private int i = 3;
 
     public static void main(String[] in){
         System.out.println("Es läuft wohooo");
+        i += 2;
+        System.out.println(i);
     }
 }
