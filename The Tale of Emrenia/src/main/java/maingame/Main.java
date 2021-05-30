c55ae8a3d541b8523ee21f857c2c93bee299637a
StellarByteStudios <kaschde2000@gmail.com>
2021-05-30 22:40:19 +0200

    Charaktererstellung läuft

--- The Tale of Emrenia/src/main/java/maingame/Main.java
+++ The Tale of Emrenia/src/main/java/maingame/Main.java
@@ -3,7 +3,6 @@ package maingame;
 import maingame.newstuff.GameEngine;
 
 public class Main {
-
     public static void main(String[] in){
         GameEngine.startGame();
     }
--- The Tale of Emrenia/src/main/java/maingame/newstuff/GameEngine.java
+++ The Tale of Emrenia/src/main/java/maingame/newstuff/GameEngine.java
@@ -6,20 +6,20 @@ public class GameEngine {
     public static void startGame(){
         Scanner scaner = new Scanner(System.in);
         System.out.println("Es läuft wohooo");
-        System.out.println("Fangen wir mit den Grundlagen an");
+        System.out.println("Fangen wir mit den Grundlagen an \n");
         Player player = new Player();
         System.out.println("Gib erstmal deinen Namen an: ");
         player.setName(scaner.nextLine());
-        System.out.println("So, nun kümmern wir uns um die Rasse");
+        System.out.println("So, nun kümmern wir uns um die Rasse\n");
         chooseRace(player);
-        System.out.println("Zum Schluss jetzt noch die Klasse");
-        player.chooseClass();
+        System.out.println("Zum Schluss jetzt noch die Klasse\n");
+        chooseClass(player);
 
-        System.out.println("Das hast du bisher jetzt erreicht");
+        System.out.println("Das hast du bisher jetzt erreicht:\n");
         System.out.println(player.toString());
         System.out.println("\n Viel Spaß damit");
     }
-
+    //* - - - Erstellung des Players - - - */
     public static void chooseRace(Player player){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Also welcher der 3 Rassen möchtest du angehören ?");
@@ -49,6 +49,40 @@ public class GameEngine {
                 return;
         }
     }
+    //Rassenwahl
+    public static void chooseClass(Player player){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Also welcher der 4 Klassen möchtest du angehören ?");
+        System.out.println("(1) Krieger \n(2) Magier \n(3) Dieb \n(4) Schütze");
+        String in = scanner.next();
+        int selection;
+        try {
+            selection = Integer.parseInt(in);
+        } catch (NumberFormatException e){
+            wrongInput();
+            chooseClass(player);
+            return;
+        }
+        switch (selection){
+            case(1):
+                player.setClass(1);
+                break;
+            case(2):
+                player.setClass(2);
+                break;
+            case(3):
+                player.setClass(3);
+                break;
+            case(4):
+                player.setClass(4);
+                break;
+            default:
+                wrongInput();
+                chooseClass(player);
+                return;
+        }
+    }
+    //Klassenwahl
     private static void wrongInput(){
         System.out.println("Kannst du mal was Gescheites eingeben du H0nda!!");
         System.out.println("Pass mal besser auf ey");
